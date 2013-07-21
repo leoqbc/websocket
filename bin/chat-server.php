b4d1b47946962e4c27e9eefb51e090e677899df5
vagrant <vagrant@php54.(none)>
2013-07-21 22:24:12 +0100

    Altera o servidor para escutar acoes do browser

--- bin/chat-server.php
+++ bin/chat-server.php
@@ -1,13 +1,15 @@
 <?php
 
 use Ratchet\Server\IoServer;
+use Ratchet\WebSocket\WsServer;
 use MyApp\Chat;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 $server = IoServer::factory(
-        new Chat(), 
-        8080
+    new WsServer(
+        new Chat()
+    ), 8080
 );
 
 $server->run();
