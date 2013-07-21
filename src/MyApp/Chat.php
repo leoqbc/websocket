d63e3fd5a6058a91f3ec1dec183fe98a34e70723
Leonardo Tumadjian <tumadjian@gmail.com>
2013-07-21 19:26:28 -0300

    Alterando saida de mensagem simples para teste
    Envia a todos os usuarios e loga no servidor, so por questao de debug

--- src/MyApp/Chat.php
+++ src/MyApp/Chat.php
@@ -27,15 +27,16 @@ class Chat implements MessageComponentInterface {
     public function onMessage(ConnectionInterface $from, $msg)
     {
         
-        $numRecv = count($this->clients) - 1;
-        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
-            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
+    #    $numRecv = count($this->clients) - 1;
+    #    echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
+    #        , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
 
         foreach ($this->clients as $client) {
-            if ($from !== $client) {
+     #       if ($from !== $client) {
                 // The sender is not the receiver, send to each client connected
+                echo $msg;
                 $client->send($msg);
-            }
+     #       }
         }
         
     }
