4d61075881e7bf8511c56db4d12f351aa761fff3
Leonardo Tumadjian <tumadjian@gmail.com>
2013-07-21 22:08:36 -0300

    Altera logica de handler do Socket que vai e vem do client
    Esta dando problemas ao fechar o navegador, não consegui que o metodo onClose faça o broadcast
    para os usuarios dando o feed de que alguém saiu de chat.

--- src/MyApp/Chat.php
+++ src/MyApp/Chat.php
@@ -10,17 +10,25 @@ class Chat implements MessageComponentInterface {
     protected $clients;
     
     public function __construct()
-    {
-        
-        $this->clients = new \SplObjectStorage();
-        
+    {  
+        $this->clients = new \SplObjectStorage();  
     }
     
     public function onOpen(ConnectionInterface $conn)
     {
         
         $this->clients->attach($conn);
-        echo "Nova conexao! ({$conn->resourceId})";
+        $texto = "Nova conexao! ({$conn->resourceId})";
+        $qtdlogin = count($this->clients);
+        foreach ($this->clients as $client) {
+            $json = <<<JSON
+            {
+                "texto":"$texto",
+                "qtdlogin":"$qtdlogin"
+            }
+JSON;
+            $client->send($json);
+        }
         
     }
     
@@ -30,29 +38,41 @@ class Chat implements MessageComponentInterface {
     #    $numRecv = count($this->clients) - 1;
     #    echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
     #        , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
-
+        $qtd = count($this->clients);
         foreach ($this->clients as $client) {
      #       if ($from !== $client) {
                 // The sender is not the receiver, send to each client connected
-                echo $msg;
-                $client->send($msg);
+                echo "Usuario $from->resourceId escreveu: " . $msg;
+                $users = '"qtdlogin":"' . $qtd . '"';
+                $client->send('{ "texto":"Usuario ' . $from->resourceId . ' escreveu: ' . $msg . '", ' . $users);
      #       }
         }
         
     }
     
-    public function onClose(ConnectionInterface $conn) {
-    
+    public function onClose(ConnectionInterface $conn) 
+    {
+        $conn->close();
         $this->clients->detach($conn);
         echo "Conexao fechada por: id: {$conn->resourceId} \n";
-        
+        $texto = "Conexao fechada por: id: {$conn->resourceId} \n";
+        $qtdlogin = count($this->clients);
+        foreach ($this->clients as $client) {
+            $json = <<<JSON
+            {
+                "texto":"$texto",
+                "qtdlogin":"$qtdlogin"
+            }
+JSON;
+            echo $json;
+            $client->send($json);
+        }  
     }
 
-    public function onError(ConnectionInterface $conn, \Exception $e) {
-        
-        echo "Oh shit! Erro encontrado, identificado como: {$e->getMessage()}\n";
-        $conn->close();
-        
+    public function onError(ConnectionInterface $conn, \Exception $e) 
+    {    
+        echo "Oh shit! Erro encontrado, identificado como: {$e->getMessage()}\n{$e->getLine()}\n";
+        $conn->close(); 
     }
     
 }
