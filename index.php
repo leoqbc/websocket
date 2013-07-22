87a45706e42b75aec741425ef9693a65677855f9
Leonardo Tumadjian <tumadjian@gmail.com>
2013-07-21 22:03:10 -0300

    Cria interface web para teste com socket em servidor

--- index.php
+++ index.php
@@ -1,13 +1,31 @@
-<?php
-
-use Ratchet\Server\IoServer;
-use MyApp\Chat;
-
-require dirname(__DIR__) . '/websockets/' . 'vendor/autoload.php';
-
-/*$server = IoServer::factory(
-        new Chat(), 
-        8080
-);*/
-
-//$server->run();
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+        <script type="text/javascript" src="js/jquery.gracefulWebSocket.js"></script>
+        <title>Página</title>
+    </head>
+    <body>
+        <script type="text/javascript">
+            var ws = $.gracefulWebSocket('ws://33.33.33.10:8080');
+            ws.onmessage = function (msg){
+                var server = eval('('+msg.data+')');
+                $('#message').prepend("<p>"+server.texto+"</p>");
+                $('#qtdusers').text(server.qtdlogin);
+            };
+            
+            $('document').ready(function() {
+                $('#send').click(function() {
+                    ws.send($("#txtin").val());
+                });
+            });
+            
+        </script>
+        <div id="message"></div>
+        <textarea id="txtin"></textarea>
+        <button id="send">enviar</button>
+        
+        <h1>Conectados no servidor: <span id="qtdusers">0</span></h1> 
+    </body>
+</html>
